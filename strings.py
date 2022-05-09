aadcdba32d756404ac2d1c548493ee577cfa2a7e
s455715 <s455715@shhac150M21.ocsb.local>
2022-05-09 12:45:25 -0400

    strings

---
+++ strings.py
@@ -0,0 +1,27 @@
+"""
+Strings are a datatype.
+Strings can be changed.
+Strings are written within single or double quotation marks.
+All user input are strings. 
+"""
+
+string1 = "this is a string "
+#string2 = 'this is also a string'
+
+print(string1)
+print(string1[5])
+print(string1[:8])
+print(string1[:4])
+print(string1[:])
+print(string1 *5)
+
+flamingo = ord('a')
+print(flamingo)
+
+flamingo2 = chr(111)
+print(flamingo2)
+#print(string2)
+
+#flamingo = input("Please enter some text: ") 
+
+#print("You entered: " + flamingo)
