b366fb830a9dd2a649bcbd1d60a8487a237c777f
s455715 <s455715@shhac150M21.ocsb.local>
2022-05-09 12:41:14 -0400

    lists

---
+++ lists.py
@@ -0,0 +1,8 @@
+oliviaList = []           # creating empty list
+
+oliviaList2 = [1, "foo", 3.3, 'x']  # creating list with elements
+
+print(oliviaList)        # prints entire list
+print(oliviaList2)       # prints entire list
+print(oliviaList2[2])    # prints 3rd element in list
+print(oliviaList2[1:3])  # prints a range of elements
