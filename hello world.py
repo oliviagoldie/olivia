156200e937b86a824338132e2dcf547363e72894
s455715 <s455715@shhac150M21.ocsb.local>
2022-05-09 12:40:31 -0400

    hello world

---
+++ hello world.py
@@ -0,0 +1,42 @@
+from pygame.examples.prevent_display_stretching import counter
+print("Hello User")
+print("Please enter a number < 8")
+print ("Choose carefully, you only have three tries:")
+
+flamingo = int(input())
+    
+    
+if True and flamingo < 8:
+    
+    print("Good Job")
+    print("Let's keep going!")
+
+if flamingo > 8:
+    print("Try again")
+    
+    print("Please enter a number <8:")
+
+flamingo = int(input())
+
+if True and flamingo < 8:
+    
+    print("Good Job")
+    
+    print("Let's keep going!")
+    
+
+if flamingo > 8:
+    print("Try again")
+    
+    print("Please enter a number <8:")
+
+flamingo = int(input())
+
+if True and flamingo < 8:
+    
+    print("Good Job")
+    
+    print("Let's keep going!")
+    
+if flamingo > 8:
+    print("I'm sorry, you failed :(")
