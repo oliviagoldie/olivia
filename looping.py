cb475258d5d439b3a2fb5bfaddf8f865c233dfc4
s455715 <s455715@shhac150M21.ocsb.local>
2022-05-09 12:42:38 -0400

    looping

---
+++ looping.py
@@ -0,0 +1,33 @@
+#for counter in range(4,8,9): 
+
+  #  print(counter)
+  #  print("hello")
+    
+    
+    
+#myName = "Olivia Goldie"
+
+
+#for letter in myName: 
+   # print(letter)
+
+"""
+this is a comment so it wont show show
+i can put whatever i want in here 
+it wont type out
+
+
+number = 1
+
+while number < 10:
+    print("hello world")
+    number = number + 1 
+    """
+    
+count = 0 
+userInput = int(input("please enter a number:"))
+
+while count < userInput:
+    print("Hello World")
+   # count += 1
+    count = count + 1
